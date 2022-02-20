0b2f933120efb93559cbb044f6a012fd29147a88
wannaSpring <chent1493@163.com>
2022-02-21 02:46:45 +0800

    feat(*): 预测模型建立、纯数据生产

--- MyQuiz.py
+++ MyQuiz.py
@@ -1,204 +1,240 @@
-from tkinter import *
-from tkinter import messagebox
-from tkinter import ttk
+import tkinter as Tk
 import csv
-
-name = ""
-maxQues = 0
-activeQues = 0
-quesType = []
-correctAnswer = []
-selectedAnswer = []
-quesDesc = []
-choice1 = []
-choice2 = []
-choice3 = []
-picFile = []
+from tkinter import messagebox
+from pubsub import pub
 
 
-def openLoginWindow():
-    global txtName
+class OtherFrame(Tk.Toplevel):
+    def __init__(self, name):
+        Tk.Toplevel.__init__(self)
+        self.geometry("400x600")
+        self.title("choose")
+        self.cvs = Tk.Canvas(self, width=400, height=600)
+        self.name = name
+        self.maxQues = 0
+        self.activeQues = 0
+        self.quesType = []
+        self.correctAnswer = []
+        self.selectedAnswer = []
+        self.quesDesc = []
+        self.choice1 = []
+        self.choice2 = []
+        self.choice3 = []
+        self.choice4 = []
+        self.choice = ''
+        self.picFile = []
+        self.btnPrev = ''
+        self.btnNext = ''
+        self.activeQues = 0
+        self.iconify()
+        self.deiconify()
 
-    loginWin.title("Login")
-    loginWin.geometry("200x100")
-    Label(loginWin, text="Name", fg="blue").grid(row=0, column=0)
-    txtName = Entry(loginWin)
-    txtName.grid(row=0, column=1, pady=10, padx=10)
-    Button(loginWin, text="Proceed", command=showMainWin).grid(row=3, column=1, columnspan=2)
+        Tk.Label(self, text="Name", fg="blue", anchor=Tk.W).place(x=0, y=0, height=25, width=50)
+        Tk.Label(self, text=self.name, anchor=Tk.W).place(x=50, y=0, height=25, width=200)
+        self.maxQues = len(open("questions.csv", "r").readlines()) - 1
+        with open("questions.csv", "r") as csvFile:
+            self.quesType = [0] * self.maxQues
+            self.correctAnswer = [""] * self.maxQues
+            self.selectedAnswer = [""] * self.maxQues
+            self.quesDesc = [""] * self.maxQues
+            self.choice1 = [""] * self.maxQues
+            self.choice2 = [""] * self.maxQues
+            self.choice3 = [""] * self.maxQues
+            self.choice4 = [""] * self.maxQues
+            self.picFile = [""] * self.maxQues
+            csvReader = csv.reader(csvFile, delimiter=':')
+            lineNo = 0
+            for row in csvReader:
+                if lineNo > 0:
+                    self.quesType[lineNo - 1] = int(row[0])
+                    self.correctAnswer[lineNo - 1] = row[1]
+                    self.quesDesc[lineNo - 1] = row[2]
+                    self.choice1[lineNo - 1] = row[3]
+                    self.choice2[lineNo - 1] = row[4]
+                    self.choice3[lineNo - 1] = row[5]
+                    self.choice4[lineNo - 1] = row[6]
+                    self.picFile[lineNo - 1] = row[7]
+                lineNo += 1
+        self.btnPrev = Tk.Button(self, text="Prev", command=self.btnPrevClick)
+        self.btnPrev.place(x=25, y=575, height=25, width=75)
+        self.btnSubmit = Tk.Button(self, text="Submit", command=self.btnSubmitClick)
+        self.btnSubmit.place(x=150, y=575, height=25, width=100)
+        self.btnNext = Tk.Button(self, text="Next", command=self.btnNextClick)
+        self.btnNext.place(x=300, y=575, height=25, width=75)
+        self.btnPrev["state"] = "disabled"
+        self.btnNext["state"] = "normal"
+        self.cvs.pack()
+        self.displayQues()
 
+    def btnPrevClick(self):
+        self.activeQues -= 1
+        self.displayQues()
 
-def showMainWin():
-    global name
-    global maxQues
-    global activeQues
-    global quesType
-    global correctAnswer
-    global selectedAnswer
-    global quesDesc
-    global choice1
-    global choice2
-    global choice3
-    global choice4
-    global picFile
-    global btnPrev
-    global btnNext
-    name = txtName.get()
+    def btnNextClick(self):
+        self.activeQues += 1
+        self.displayQues()
 
-    activeQues = 0
-    loginWin.destroy()
-    tkw.iconify()
-    tkw.deiconify()
+    def displayQues(self):
+        global btnPrev
+        global btnNext
+        global labImg
+        global choice
+        curQuesNoDesc = "QUESTION " + str(self.activeQues + 1) + " of " + str(self.maxQues)
+        curQuesDesc = self.quesDesc[self.activeQues]
+        Tk.Label(self, text=curQuesNoDesc, font=("Arial 12 bold")) \
+            .place(x=50, y=50, height=25, width=300)
+        Tk.Label(self, text=curQuesDesc, font=("Arial 10 bold"), anchor=Tk.W) \
+            .place(x=50, y=75, height=50, width=300)
+        self.cvs.delete("old")
+        try:
+            labImg
+        except NameError:
+            print()
+        else:
+            labImg.image = None
+            labImg.destroy()
+        picOffset = 0
+        if ('img/' in self.picFile[self.activeQues]):
+            picOffset = 200
+            picName = self.picFile[self.activeQues].replace('img/', '')
+            quesImg = Tk.PhotoImage(file="./images/" + picName)
+            labImg = Tk.Label(self, image=quesImg)
+            labImg.image = quesImg
+            labImg.place(x=50, y=150, height=150, width=150)
 
-    Label(tkw, text="Name", fg="blue", anchor=W).place(x=0, y=0, height=25, width=50)
-    Label(tkw, text=name, anchor=W).place(x=50, y=0, height=25, width=200)
-    maxQues = len(open("questions.csv", "r").readlines()) - 1
-    with open("questions.csv", "r") as csvFile:
-        quesType = [0] * maxQues
-        correctAnswer = [""] * maxQues
-        selectedAnswer = [""] * maxQues
-        quesDesc = [""] * maxQues
-        choice1 = [""] * maxQues
-        choice2 = [""] * maxQues
-        choice3 = [""] * maxQues
-        choice4 = [""] * maxQues
-        picFile = [""] * maxQues
-        csvReader = csv.reader(csvFile, delimiter=':')
-        lineNo = 0
-        for row in csvReader:
-            if lineNo > 0:
-                quesType[lineNo - 1] = int(row[0])
-                correctAnswer[lineNo - 1] = row[1]
-                quesDesc[lineNo - 1] = row[2]
-                choice1[lineNo - 1] = row[3]
-                choice2[lineNo - 1] = row[4]
-                choice3[lineNo - 1] = row[5]
-                choice4[lineNo - 1] = row[6]
-                picFile[lineNo - 1] = row[7]
-            lineNo += 1
-    btnPrev = Button(tkw, text="Prev", command=btnPrevClick)
-    btnPrev.place(x=25, y=575, height=25, width=75)
-    btnSubmit = Button(tkw, text="Submit", command=btnSubmitClick)
-    btnSubmit.place(x=150, y=575, height=25, width=100)
-    btnNext = Button(tkw, text="Next", command=btnNextClick)
-    btnNext.place(x=300, y=575, height=25, width=75)
-    btnPrev["state"] = "disabled"
-    btnNext["state"] = "normal"
-    cvs.pack()
-    displayQues()
+        choice1Desc = "(A)  " + self.choice1[self.activeQues]
+        choice2Desc = "(B)  " + self.choice2[self.activeQues]
+        choice3Desc = "(C)  " + self.choice3[self.activeQues]
+        choice4Desc = "(D)  " + self.choice4[self.activeQues]
+        self.choice = Tk.StringVar()
+        self.choice.set(None)
+        if (self.selectedAnswer[self.activeQues] == "A"):
+            self.choice.set("A")
+        elif (self.selectedAnswer[self.activeQues] == "B"):
+            self.choice.set("B")
+        elif (self.selectedAnswer[self.activeQues] == "C"):
+            self.choice.set("C")
+        elif (self.selectedAnswer[self.activeQues] == "D"):
+            self.choice.set("D")
+        radChoice1 = Tk.Radiobutton(self, text=choice1Desc, variable=self.choice, value="A", command=self.choiceClick)
+        radChoice1.place(x=50, y=(150 + picOffset), height=50, width=300)
+        radChoice2 = Tk.Radiobutton(self, text=choice2Desc, variable=self.choice, value="B", command=self.choiceClick)
+        radChoice2.place(x=50, y=(200 + picOffset), height=50, width=300)
+        radChoice3 = Tk.Radiobutton(self, text=choice3Desc, variable=self.choice, value="C", command=self.choiceClick)
+        radChoice3.place(x=50, y=(350 + picOffset), height=50, width=300)
+        radChoice4 = Tk.Radiobutton(self, text=choice4Desc, variable=self.choice, value="D", command=self.choiceClick)
+        radChoice4.place(x=50, y=(550 + picOffset), height=50, width=300)
+        self.cvs.create_window(50, (150 + picOffset), window=radChoice1, tags="old", anchor=Tk.W)
+        self.cvs.create_window(50, (200 + picOffset), window=radChoice2, tags="old", anchor=Tk.W)
+        self.cvs.create_window(50, (250 + picOffset), window=radChoice3, tags="old", anchor=Tk.W)
+        self.cvs.create_window(50, (300 + picOffset), window=radChoice4, tags="old", anchor=Tk.W)
 
+        if (self.activeQues == 0):
+            self.btnPrev["state"] = "disabled"
+            self.btnNext["state"] = "normal"
+        elif ((self.activeQues + 1) == self.maxQues):
+            self.btnPrev["state"] = "normal"
+            self.btnNext["state"] = "disabled"
+        else:
+            self.btnPrev["state"] = "normal"
+            self.btnNext["state"] = "normal"
+        self.cvs.pack()
 
-def btnPrevClick():
-    global activeQues
+    def choiceClick(self):
+        self.selectedAnswer[self.activeQues] = self.choice.get()
 
-    activeQues -= 1
-    displayQues()
+    def btnSubmitClick(self):
+        reply = messagebox.askquestion("Confirm", "Confirm Submitting (Y/N)?")
+        if (reply == "yes"):
+            self.submitAnswer()
 
-
-def btnNextClick():
-    global activeQues
-
-    activeQues += 1
-    displayQues()
-
-
-def displayQues():
-    global btnPrev
-    global btnNext
-    global labImg
-    global choice
-
-    curQuesNoDesc = "QUESTION " + str(activeQues + 1) + " of " + str(maxQues)
-    curQuesDesc = quesDesc[activeQues]
-    Label(tkw, text=curQuesNoDesc, font=("Arial 12 bold")) \
-        .place(x=50, y=50, height=25, width=300)
-    Label(tkw, text=curQuesDesc, font=("Arial 10 bold"), anchor=W) \
-        .place(x=50, y=75, height=50, width=300)
-    cvs.delete("old")
-    try:
-        labImg
-    except NameError:
-        print()
-    else:
-        print(labImg)
-        labImg.image = None
-        labImg.destroy()
-    picOffset = 0
-    if ('img/' in picFile[activeQues]):
-        picOffset = 200
-        picName = picFile[activeQues].replace('img/', '')
-        print(picName)
-        quesImg = PhotoImage(file="./images/" + picName)
-        labImg = ttk.Label(tkw, image=quesImg)
-        labImg.image = quesImg
-        labImg.place(x=50, y=150, height=150, width=150)
-
-    choice1Desc = "(A)  " + choice1[activeQues]
-    choice2Desc = "(B)  " + choice2[activeQues]
-    choice3Desc = "(C)  " + choice3[activeQues]
-    choice4Desc = "(D)  " + choice4[activeQues]
-    choice = StringVar()
-    choice.set(None)
-    if (selectedAnswer[activeQues] == "A"):
-        choice.set("A")
-    elif (selectedAnswer[activeQues] == "B"):
-        choice.set("B")
-    elif (selectedAnswer[activeQues] == "C"):
-        choice.set("C")
-    elif (selectedAnswer[activeQues] == "D"):
-        choice.set("D")
-    radChoice1 = Radiobutton(tkw, text=choice1Desc, variable=choice, value="A", command=choiceClick)
-    radChoice1.place(x=50, y=(150 + picOffset), height=50, width=300)
-    radChoice2 = Radiobutton(tkw, text=choice2Desc, variable=choice, value="B", command=choiceClick)
-    radChoice2.place(x=50, y=(200 + picOffset), height=50, width=300)
-    radChoice3 = Radiobutton(tkw, text=choice3Desc, variable=choice, value="C", command=choiceClick)
-    radChoice3.place(x=50, y=(350 + picOffset), height=50, width=300)
-    radChoice4 = Radiobutton(tkw, text=choice4Desc, variable=choice, value="D", command=choiceClick)
-    radChoice4.place(x=50, y=(550 + picOffset), height=50, width=300)
-    cvs.create_window(50, (150 + picOffset), window=radChoice1, tags="old", anchor=W)
-    cvs.create_window(50, (200 + picOffset), window=radChoice2, tags="old", anchor=W)
-    cvs.create_window(50, (250 + picOffset), window=radChoice3, tags="old", anchor=W)
-    cvs.create_window(50, (300 + picOffset), window=radChoice4, tags="old", anchor=W)
-
-    if (activeQues == 0):
-        btnPrev["state"] = "disabled"
-        btnNext["state"] = "normal"
-    elif ((activeQues + 1) == maxQues):
-        btnPrev["state"] = "normal"
-        btnNext["state"] = "disabled"
-    else:
-        btnPrev["state"] = "normal"
-        btnNext["state"] = "normal"
-    cvs.pack()
+    def submitAnswer(self):
+        pub.sendMessage('submit', answer=self.selectedAnswer)
 
 
-def choiceClick():
-    selectedAnswer[activeQues] = choice.get()
+class ApplicationForm:
+    def __init__(self):
+        self.name = ''
+        self.age = 0
+        self.gender = 0
+        self.highestQualifications = ''
+        self.familyToMove = False
+        self.economicLevel = ''
 
 
-def btnSubmitClick():
-    reply = messagebox.askquestion("Confirm", "Confirm Submitting (Y/N)?")
-    if (reply == "yes"):
-        submitAnswer()
+class MyApp(object):
+    def __init__(self, parent):
+        """Constructor"""
+        self.root = parent
+        self.root.title("Login")
+        self.frame = Tk.Frame(parent)
+        self.applicationForm = ApplicationForm()
+
+        Tk.Label(self.root, text="Name:", fg="blue").grid(row=0, sticky='E')
+        self.applicationForm.name = Tk.Entry(self.root)
+        self.applicationForm.name.grid(row=0, column=1)
+
+        Tk.Label(self.root, text="Age:", fg="blue").grid(row=1, sticky='E')
+        self.applicationForm.age = Tk.Entry(self.root)
+        self.applicationForm.age.grid(row=1, column=1, padx=10, pady=10)
+
+        Tk.Label(self.root, text="Gender:", fg="blue").grid(row=2, sticky='E')
+        radioValue = Tk.IntVar()
+        rdioOne = Tk.Radiobutton(self.root, text='male', variable=radioValue, value=1)
+        rdioTwo = Tk.Radiobutton(self.root, text='female', variable=radioValue, value=0)
+        self.applicationForm.gender = radioValue
+        rdioOne.grid(row=2, column=1)
+        rdioTwo.grid(row=2, column=1, sticky='W')
+
+        Tk.Label(self.root, text="Qualifications:", fg="blue").grid(row=3, sticky='E')
+        self.applicationForm.highestQualifications = Tk.Entry(self.root)
+        self.applicationForm.highestQualifications.grid(row=3, column=1, padx=10, pady=10)
+
+        Tk.Label(self.root, text="Family want move?", fg="blue").grid(row=4, sticky='E')
+        familyValue = Tk.IntVar()
+        familyRdioOne = Tk.Radiobutton(self.root, text='true', variable=familyValue, value=False)
+        familyRdioTwo = Tk.Radiobutton(self.root, text='false', variable=familyValue, value=True)
+        self.applicationForm.familyToMove = familyValue
+        familyRdioOne.grid(row=4, column=1)
+        familyRdioTwo.grid(row=4, column=1, sticky='W')
+
+        Tk.Label(self.root, text="EconomicLevel:", fg="blue").grid(row=5, sticky='E')
+        self.applicationForm.economicLevel = Tk.Entry(self.root)
+        self.applicationForm.economicLevel.grid(row=5, column=1, padx=10, pady=10)
+
+        Tk.Button(self.root, text="Proceed", command=self.openFrame).grid(row=6, column=1, columnspan=2)
+
+        pub.subscribe(self.writeFile, 'submit')
+
+    def writeFile(self, answer):
+        newAnswer = [self.applicationForm.name.get(), self.applicationForm.age.get(), self.applicationForm.gender.get(),
+                     self.applicationForm.highestQualifications.get(), self.applicationForm.familyToMove.get(),
+                     self.applicationForm.economicLevel.get()] + answer
+        with open("answer.csv", "a", newline="") as csvFile:
+            csvWriter = csv.writer(csvFile, delimiter=':')
+            csvWriter.writerow(newAnswer)
+        exit()
+
+    def hide(self):
+        self.root.withdraw()
+
+    def openFrame(self):
+        self.hide()
+        subFrame = OtherFrame(self.applicationForm.name.get())
+
+    # ----------------------------------------------------------------------
+    def onCloseOtherFrame(self, otherFrame):
+        otherFrame.destroy()
+        # 过会再看是否保留
+        self.show()
+
+    def show(self):
+        self.root.update()
+        self.root.deiconify()
 
 
-def submitAnswer():
-    savedAnswers = [""] * (maxQues + 1)
-    savedAnswers[0] = name
-    with open("answer.txt", "a", newline="") as csvFile:
-        csvWriter = csv.writer(csvFile, delimiter=':')
-        for k in range(1, (maxQues + 1)):
-            savedAnswers[k] = selectedAnswer[k - 1]
-        csvWriter.writerow(savedAnswers)
-    exit()
-
-
-tkw = Tk()
-tkw.title("Knowledge Test")
-cvs = Canvas(tkw, width=400, height=600)
-tkw.withdraw()
-
-loginWin = Toplevel(tkw)
-openLoginWindow()
-
-cvs.pack()
-tkw.mainloop()
+if __name__ == "__main__":
+    root = Tk.Tk()
+    root.geometry("300x300")
+    app = MyApp(root)
+    root.mainloop()
--- MyResult.py
+++ MyResult.py
@@ -1,63 +1,93 @@
 from tkinter import *
 import csv
-
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
 
 stuName = []
+stuAge = []
+stuGender = []
 maxQues = 0
 maxStu = 0
 activeStu = 0
 correctAnswer = []
+correctCountList = []
 selectedAnswer = []
 result = []
 
 
 def showMainWin():
     global stuName
+    global stuAge
+    global stuGender
+    global lpltName
     global maxQues
     global maxStu
     global correctAnswer
+    global correctCountList
     global selectedAnswer
     global result
     global btnPrev
     global btnNext
+    global btnGraph
+    global btnStats
 
-    maxQues = len(open("questions.txt").readlines()) - 1
+    maxQues = len(open("questions.csv").readlines()) - 1
     correctAnswer = [""] * maxQues
-    with open("questions.txt") as csvFile:
+    with open("questions.csv") as csvFile:
         csvReader = csv.reader(csvFile, delimiter=':')
         lineNo = 0
         for row in csvReader:
             if (lineNo > 0):
-                correctAnswer[lineNo-1] = row[1]
+                correctAnswer[lineNo - 1] = row[1]
             lineNo += 1
 
-    maxStu = len(open("answer.txt").readlines())
+    maxStu = len(open("answer.csv").readlines())
     stuName = [""] * maxStu
+    stuAge = [""] * maxStu
+    stuGender = [""] * maxStu
     selectedAnswer = [""] * maxStu
     result = [""] * maxStu
-    with open("answer.txt") as csvFile:
+    correctCountList = [""] * maxStu
+    with open("answer.csv") as csvFile:
         csvReader = csv.reader(csvFile, delimiter=':')
         lineNo = 0
+
+        lpltName = []
+        lpltAge = []
+        lpltGender = []
+
         for row in csvReader:
             stuName[lineNo] = row[0]
+            stuAge[lineNo] = row[1]
+            stuGender[lineNo] = row[2]
+
+            lpltName.append(stuName[lineNo])
+            lpltAge.append(stuAge[lineNo])
+            lpltGender.append(stuGender[lineNo])
+
             strAns = ""
             strResult = ""
+            correctCount = 0
             for ans in range(maxQues):
-                strAns = strAns +  row[ans+1]
-                if (row[ans+1] == correctAnswer[ans]):
+                strAns = strAns + row[ans + 6]
+                if (row[ans + 6] == correctAnswer[ans]):
                     strResult = strResult + "1"
+                    correctCount += 1
                 else:
                     strResult = strResult + "0"
             selectedAnswer[lineNo] = strAns
+            correctCountList[lineNo] = correctCount
             result[lineNo] = strResult
             lineNo += 1
-
     btnPrev = Button(tkw, text="Prev", command=btnPrevClick)
     btnPrev.place(x=25, y=575, height=25, width=75)
     btnQuit = Button(tkw, text="Quit", command=btnQuitClick)
     btnQuit.place(x=150, y=575, height=25, width=100)
     btnNext = Button(tkw, text="Next", command=btnNextClick)
     btnNext.place(x=300, y=575, height=25, width=75)
+    btnGraph = Button(tkw, text="Graph", command=btnGraphClick)
+    btnGraph.place(x=280, y=525, height=25, width=100)
     btnPrev["state"] = "disabled"
     btnNext["state"] = "normal"
     cvs.pack()
@@ -67,14 +97,13 @@ def showMainWin():
 def btnPrevClick():
     global activeStu
 
-    activeStu-=1
+    activeStu -= 1
     displayResult()
 
 
 def btnNextClick():
     global activeStu
-
-    activeStu+=1
+    activeStu += 1
     displayResult()
 
 
@@ -82,31 +111,44 @@ def displayResult():
     global btnPrev
     global btnNext
 
-    curStuDesc = "Student " + str(activeStu+1) + " of " + str(maxStu)
-    curStuName = "NAME : " + stuName[activeStu]
-    Label(tkw, text=curStuDesc, font=("Arial 12 bold"))\
+    curStuDesc = "Student " + str(activeStu + 1) + " of " + str(maxStu)
+    curStuName = "Name: " + stuName[activeStu]
+    curStuAge = "Age: " + stuAge[activeStu]
+    curStuGender = "Gender: " + stuGender[activeStu]
+    Label(tkw, text=curStuDesc, font=("Arial 12 bold"), anchor=W) \
         .place(x=50, y=50, height=25, width=300)
-    Label(tkw, text=curStuName, font=("Arial 12 bold"))\
+    Label(tkw, text=curStuName, font=("Arial 12 bold"), anchor=W) \
         .place(x=50, y=75, height=25, width=300)
+    Label(tkw, text=curStuAge, font=("Arial 12 bold"), anchor=W) \
+        .place(x=50, y=100, height=25, width=300)
+    Label(tkw, text=curStuGender, font=("Arial 12 bold"), anchor=W) \
+        .place(x=50, y=125, height=25, width=300)
 
     strHeader = "NO  SELECTED  ANSWER  RESULT"
     Label(tkw, text=strHeader, font=("Arial 10 bold"), anchor=W) \
-        .place(x=50, y=125, height=25, width=300)
-    startY = 150
+        .place(x=50, y=150, height=25, width=300)
+    startY = 175
     countCorrect = 0
     for k in range(maxQues):
-        strResult = str(k+1) + ".          " + (selectedAnswer[activeStu])[k] + "              "
+        strResult = str(k + 1) + ".          " + (selectedAnswer[activeStu])[k] + "              "
         strResult = strResult + correctAnswer[k] + "           "
         if ((result[activeStu])[k] == "1"):
             strResult = strResult + "CORRECT"
-            countCorrect+=1
+            countCorrect += 1
         else:
             strResult = strResult + "WRONG"
-        Label(tkw, text=strResult, font=("Arial 10 bold"), anchor=W)\
+        Label(tkw, text=strResult, font=("Arial 10 bold"), anchor=W) \
             .place(x=50, y=(startY + 25 * k), height=25, width=300)
+    correctCountList[activeStu] = countCorrect
     strStat = str(countCorrect) + "/" + str(maxQues)
-    Label(tkw, text=strStat, anchor=W)\
+    Label(tkw, text=strStat, anchor=W) \
         .place(x=50, y=(startY + 25 * maxQues), height=25, width=300)
+    if countCorrect >= 8:
+        strGrade = "Grade: PASS"
+    else:
+        strGrade = "Grade: FAIL"
+    Label(tkw, text=strGrade, anchor=W) \
+        .place(x=100, y=(startY + 25 * maxQues), height=25, width=300)
 
     if (activeStu == 0):
         btnPrev["state"] = "disabled"
@@ -120,12 +162,28 @@ def displayResult():
     cvs.pack()
 
 
+def btnGraphClick():
+    average = np.mean(correctCountList)
+    max = np.max(correctCountList)
+    min = np.min(correctCountList)
+    plt.subplot(2, 1, 1)
+    plt.title("Quiz Marks")
+    plt.xlabel("Name")
+    plt.ylabel("Score")
+    plt.ylim(0, 11)
+    plt.plot(lpltName, correctCountList, "rs-")
+    plt.subplot(2, 1, 2)
+    plt.xlabel("Gender")
+    plt.ylim(0, 11)
+    plt.bar(["Average", "max", "min"], [average, max, min], color="blue")
+    plt.show()
+
 def btnQuitClick():
     exit()
 
 
 tkw = Tk()
-tkw.title("Test Result")
+tkw.title("English Test Result")
 cvs = Canvas(tkw, width=400, height=600)
 showMainWin()
 tkw.mainloop()
